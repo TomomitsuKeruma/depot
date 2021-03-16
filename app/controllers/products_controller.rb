3e1692cce21073ec8dab919f5a271d7709cf10fc
TomomitsuKeruma <78133385+TomomitsuKeruma@users.noreply.github.com>
2021-03-16 15:29:14 +0900

    商品追加機能実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -64,6 +64,6 @@ class ProductsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def product_params
-      params.fetch(:product, {})
+      params.require(:product).permit(:title, :description, :image_url, :price)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_15_125548) do
+ActiveRecord::Schema.define(version: 2021_03_16_030537) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
